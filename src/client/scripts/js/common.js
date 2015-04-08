17af98e8b1c2377cda2cd1f393a5ecd35d6b6113
Magamed <a1essar@yandex.ru>
2015-04-08 18:36:43 +0300

    Update 2015-04-08T15:36:37.978Z

--- src/client/scripts/js/common.js
+++
@@ -1,5 +0,0 @@
-requirejs(['app'], function(app){
-    console.log('%cfile: common.js', 'color: #C2ECFF');
-
-    new app();
-});
--- src/client/scripts/js/test/utils.js
+++
@@ -1,7 +0,0 @@
-define(['chai', 'utils'], function(chai, utils) {
-    describe('Testing "utils.js"', function(done){
-        it('utils.test()', function(){
-            chai.expect(utils.test()).to.be.not.ok();
-        });
-    });
-});
